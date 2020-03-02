0e61d84cce2971668763103ccda15fe8c609575b
Ruben Costa <ruben@civist.com>
2020-03-02 17:34:54 +0100

    base reproduction

---
+++ src/routes/_error.svelte
@@ -0,0 +1,55 @@
+<script>
+  export let status;
+  export let error;
+  import Nav from "../components/Nav.svelte";
+
+  const dev = process.env.NODE_ENV === "development";
+
+  let showNav = false;
+</script>
+
+<style>
+  h1,
+  p {
+    margin: 0 auto;
+  }
+
+  h1 {
+    font-size: 2.8em;
+    font-weight: 700;
+    margin: 0 0 0.5em 0;
+  }
+
+  p {
+    margin: 1em auto;
+  }
+
+  @media (min-width: 480px) {
+    h1 {
+      font-size: 4em;
+    }
+  }
+</style>
+
+<svelte:head>
+  <title>{status}</title>
+</svelte:head>
+
+<button
+  on:click={() => {
+    showNav = true;
+  }}>
+  Show
+</button>
+
+{#if showNav}
+  <Nav />
+{/if}
+
+<h1>{status}</h1>
+
+<p>{error.message}</p>
+
+{#if dev && error.stack}
+  <pre>{error.stack}</pre>
+{/if}
